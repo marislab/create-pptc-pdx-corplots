8db941260e76c9ae3c5ab25e360e3e13fc87dcb3
Khushbu Patel <kpatel427@gatech.edu>
2019-03-07 10:17:54 -0500

    update

--- VAF-corr-plots-docker/pipeline.sh
+++ VAF-corr-plots-docker/pipeline.sh
@@ -14,6 +14,18 @@
 # Requirement: Python 3.6
 # Usage: ./pipeline.sh
 
+echo "Fetching data files..."
+mkdir ~/data/
+cd ~/data/
+
+# 1. Download DNA MAF file
+wget --output-document='2019-02-14-allpdx-clean-maf-240.rda' https://ndownloader.figshare.com/files/14414198
+
+# 2. Download RNA MAF file
+#wget --output-document='' https://ndownloader.figshare.com/files/14414198
+
+# 3. Clinical file
+wget --output-document='pptc-pdx-clinical-web.txt' https://ndownloader.figshare.com/files/14508536
 
 echo "Pulling the data from MAF file..."
 
